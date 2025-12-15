38fa19044621362bfd1570d04fd181352052fcbe
Aryosetowmn <aryosetowmnwork@gmail.com>
2025-12-15 23:17:27 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/staff', function () {
+    return view('staff');
 });
