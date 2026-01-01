3bbca06bba2930f7f50504c698826996ce2033b4
Aryosetowmn <aryosetowmnwork@gmail.com>
2026-01-01 16:45:03 +0700

    resetting up project

--- src/boot/google-login.js
+++
@@ -1,8 +0,0 @@
-import { boot } from 'quasar/wrappers'
-import vue3GoogleLogin from 'vue3-google-login'
-
-export default boot(({ app }) => {
-  app.use(vue3GoogleLogin, {
-    clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID
-  })
-})
